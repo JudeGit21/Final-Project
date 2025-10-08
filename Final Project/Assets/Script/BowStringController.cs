1df294cd5c671302053f434b65e8a55c743450a9
JudeGit21 <jude.ehiwe@hyperisland.se>
2025-10-09 01:06:30 +0200

    assign bow controller script

--- Final Project/Assets/Script/BowStringController.cs
+++ Final Project/Assets/Script/BowStringController.cs
@@ -3,16 +3,13 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class BowStringController : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField] 
     private BowString bowStringRenderer;
 
     private XRGrabInteractable interactable;
 
-    [SerializeField]
-    private Transform midPointGrabObject, midPointVisualObject, midPointParent;
-
-    [SerializeField]
-    private float bowStringStretchLimit = 0.3f;
+    [SerializeField] 
+    private Transform midPointGrabObject;
 
     private Transform interactor;
 
@@ -31,7 +28,6 @@ public class BowStringController : MonoBehaviour
     {
         interactor = null;
         midPointGrabObject.localPosition = Vector3.zero;
-        midPointVisualObject.localPosition = Vector3.zero;
         bowStringRenderer.CreateString(null);
     }
 
@@ -44,46 +40,7 @@ public class BowStringController : MonoBehaviour
     {
         if (interactor != null)
         {
-            // Convert bow string midpoint position to local space of the parent
-            Vector3 midPointLocalSpace = midPointParent.InverseTransformPoint(midPointGrabObject.position);
-
-            // Get the absolute Z offset
-            float midPointLocalZAbs = Mathf.Abs(midPointLocalSpace.z);
-
-            // Handle bowstring logic
-            HandleStringPushedBackToStart(midPointLocalSpace);
-            HandleStringPulledBackToLimit(midPointLocalZAbs, midPointLocalSpace);
-            HandlePullingString(midPointLocalZAbs, midPointLocalSpace);
-
-            // Update the visual bowstring
-            bowStringRenderer.CreateString(midPointVisualObject.position);
-        }
-    }
-
-    private void HandlePullingString(float midPointLocalZAbs, Vector3 midPointLocalSpace)
-    {
-        // What happens when pulling the string within limits
-        if (midPointLocalSpace.z < 0 && midPointLocalZAbs < bowStringStretchLimit)
-        {
-            midPointVisualObject.localPosition = new Vector3(0, 0, midPointLocalSpace.z);
-        }
-    }
-
-    private void HandleStringPulledBackToLimit(float midPointLocalZAbs, Vector3 midPointLocalSpace)
-    {
-        // Restrict maximum pulling distance
-        if (midPointLocalSpace.z < 0 && midPointLocalZAbs >= bowStringStretchLimit)
-        {
-            midPointVisualObject.localPosition = new Vector3(0, 0, -bowStringStretchLimit);
-        }
-    }
-
-    private void HandleStringPushedBackToStart(Vector3 midPointLocalSpace)
-    {
-        // Reset the string when not being pulled
-        if (midPointLocalSpace.z > 0)
-        {
-            midPointVisualObject.localPosition = Vector3.zero;
+            bowStringRenderer.CreateString(midPointGrabObject.transform.position);
         }
     }
 }
